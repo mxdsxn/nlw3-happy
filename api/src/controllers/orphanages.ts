162690e368b38645225c1f2ca6fafcd025ebaeca
Madson Santana <madson.santana96@gmail.com>
2020-10-14 18:10:28 -0300

    feat(api): validacoes e exceção

--- api/src/controllers/orphanages.ts
+++ api/src/controllers/orphanages.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from 'express'
 import { getRepository } from 'typeorm'
+import * as Yup from 'yup'
 
 import Orphanages from '../models/orphanages'
 
@@ -25,6 +26,8 @@ export default {
     return res.status(200).json(orphanages.map(orphanage => orphanage.view()))
   },
   async create(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanages)
+
     const {
       name,
       latitude,
@@ -40,9 +43,8 @@ export default {
     const images = requestImages.map(image => {
       return { path: image.filename }
     })
-    const orphanagesRepository = getRepository(Orphanages)
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -51,7 +53,28 @@ export default {
       opening_hours,
       open_on_weekend,
       images
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().max(300).required(),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekend: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     })
+
+    await schema.validate(data, {
+      abortEarly: false
+    })
+
+    const orphanage = orphanagesRepository.create(data)
 
     await orphanagesRepository.save(orphanage)
 
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,17 @@
 import express from 'express'
+import 'express-async-errors'
 import path from 'path'
 
 import './database/connection'
 import { routes } from './routes'
+import errorHandler from './errors/handler'
 
 const server = express()
 
 server.use(express.json())
 server.use(routes)
 server.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
-
+server.use(errorHandler)
 server.get('/', (req, res) => (res.json({ mensagem: 'rodando server' })))
 
 server.listen(3333)
